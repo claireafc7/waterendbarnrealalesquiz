9db3bdc1fde5234eb49e768ee550841b98f863db
claireafc7 <clairequinn97123@gmail.com>
2024-08-07 17:07:11 +0100

    Add files via upload

--- ales.js
+++ ales.js
@@ -3,7 +3,7 @@ export const ales = [
     {
         name: "Abbot Ale",
         strengthCategory: 5.0,
-        strength: "Between 4.5% ABV and 5.5% ABV",
+        strength: "Between 4.6% ABV and 5.5% ABV",
         colour: "Amber",
         flavour: ["Fruity", "Malty", "Rich"]
     },
@@ -56,5 +56,414 @@ export const ales = [
         strength: "Between 3.5% ABV and 4.5% ABV",
         colour: "Golden",
         flavour: ["Zesty", "Hoppy", "Fruity"]
+    },
+    
+  {
+        name: "Hobgoblin Gold",
+        strengthCategory: 4.2,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Refreshing", "Malty", "Zesty"]
+    },
+        
+    {
+        name: "Ghost Ship",
+        strengthCategory: 4.5,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Citrusy", "Hoppy", "Refreshing"]
+    },
+    
+        {
+        name: "Rev James Original",
+        strengthCategory: 4.5,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Ruby",
+        flavour: ["Rich", "Malty", "Refreshing"]
+    },
+    
+        {
+        name: "Exmoor Gold",
+        strengthCategory: 4.5,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Malty", "Citrusy", "Toffee"]
+    },
+    
+        {
+        name: "Old Speckled Hen",
+        strengthCategory: 4.5,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Amber",
+        flavour: ["Malty", "Smooth", "Fruity"]
+    },
+    
+        {
+        name: "Hobgoblin Ruby",
+        strengthCategory: 4.5,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Ruby",
+        flavour: ["Toffee", "Chocolatey", "Full Bodied"]
+    },
+    
+        {
+        name: "Fire Island Buffalo",
+        strengthCategory: 4.6,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Ruby",
+        flavour: ["Spicy", "Full Bodied", "Fruity"]
+    },
+    
+        {
+        name: "Old Hooky",
+        strengthCategory: 4.6,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Amber",
+        flavour: ["Fruity", "Malty", "Sweet"]
+    },
+    
+        {
+        name: "Broadside",
+        strengthCategory: 4.7,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Ruby",
+        flavour: ["Fruitcake", "Full Bodied", "Malty"]
+    },
+    
+        {
+        name: "Fortyniner",
+        strengthCategory: 4.9,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Malty", "Bitter", "Sweet"]
+    },
+    
+        {
+        name: "Plum Porter",
+        strengthCategory: 4.9,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Ruby",
+        flavour: ["Fruity", "Full Bodied", "Hoppy"]
+    },
+    
+        {
+        name: "Sea Fury",
+        strengthCategory: 5.0,
+        strength: "Between 4.65% ABV and 5.5% ABV",
+        colour: "Ruby",
+        flavour: ["Malty", "Hoppy", "Fruity"]
+    },
+    
+        {
+        name: "Old Peculier",
+        strengthCategory: 5.6,
+        strength: "Above 5.5% ABV",
+        colour: "Deep Ruby, Black",
+        flavour: ["Rich", "Fruity", "Smooth"]
+    },
+    
+        {
+        name: "Old Empire IPA",
+        strengthCategory: 5.7,
+        strength: "Above 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Citrusy", "Hoppy", "Bitter"]
+    },
+    
+        {
+        name: "Jaipur",
+        strengthCategory: 5.9,
+        strength: "Above 5.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Smooth", "Hoppy", "Fruity"]
+    },
+    
+        {
+        name: "Abbot Reserve",
+        strengthCategory: 6.5,
+        strength: "Above 5.5% ABV",
+        colour: "Amber",
+        flavour: ["Full Bodied", "Rich", "Fruity"]
+    },
+    
+        {
+        name: "Bitter & Twisted",
+        strengthCategory: 3.8,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Citrusy", "Zesty", "Fruity"]
+    },
+    
+        {
+        name: "Anthem",
+        strengthCategory: 3.8,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Refreshing", "Citrusy", "Fruity"]
+    },
+    
+        {
+        name: "Bear Ass",
+        strengthCategory: 4.0,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Ruby",
+        flavour: ["Smooth", "Malty", "Fruity"]
+    },
+    
+        {
+        name: "Wallops Wood",
+        strengthCategory: 4.0,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Amber",
+        flavour: ["Malty", "Hoppy"]
+    },
+    
+        {
+        name: "SW19",
+        strengthCategory: 4.0,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Citrusy", "Zesty", "Refreshing"]
+    },
+    
+        {
+        name: "Farmers Brown Cow",
+        strengthCategory: 4.2,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Ruby",
+        flavour: ["Rich", "Smooth", "Citrusy"]
+    },
+    
+        {
+        name: "Time Bandit",
+        strengthCategory: 4.2,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Citrusy", "Hoppy", "Refreshing"]
+    },
+    
+        {
+        name: "Hullabaloo",
+        strengthCategory: 4.2,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Amber",
+        flavour: ["Biscuit", "Smooth", "Malty"]
+    },
+    
+        {
+        name: "Citra Pale ALe",
+        strengthCategory: 4.2,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Citrusy", "Refreshing", "Zesty"]
+    },
+    
+        {
+        name: "Atlantic",
+        strengthCategory: 4.2,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Citrusy", "Hoppy", "Fruity"]
+    },
+    
+        {
+        name: "Mayflower Session IPA",
+        strengthCategory: 4.2,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Bitter", "Hoppy", "Citrusy"]
+    },
+    
+        {
+        name: "SA Gold",
+        strengthCategory: 4.3,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Refreshing", "Hoppy", "Citrusy"]
+    },
+    
+        {
+        name: "Coyote",
+        strengthCategory: 4.3,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Amber",
+        flavour: ["Caramel", "Hoppy", "Malty"]
+    },
+    
+        {
+        name: "Decadence",
+        strengthCategory: 4.4,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Zesty", "Light", "Roasted"]
+    },
+    
+        {
+        name: "Jemima's Pitchfork",
+        strengthCategory: 4.5,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Refreshing", "Hoppy", "Fruity"]
+    },
+    
+        {
+        name: "Lancaster Black",
+        strengthCategory: 4.5,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Deep Ruby, Black",
+        flavour: ["Chocolatey", "Smooth", "Floral"]
+    },
+    
+        {
+        name: "White Star",
+        strengthCategory: 4.5,
+        strength: "Between 3.5% ABV and 4.5% ABV",
+        colour: "Golden",
+        flavour: ["Citrusy", "Hoppy", "Fruity"]
+    },
+    
+        {
+        name: "Bishops Farewell",
+        strengthCategory: 4.6,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Smooth", "Refreshing", "Fruity"]
+    },
+    
+        {
+        name: "Dark Side of the Moose",
+        strengthCategory: 4.6,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Ruby",
+        flavour: ["Rich", "Malty", "Toffee"]
+    },
+    
+        {
+        name: "Trooper",
+        strengthCategory: 4.7,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Malty", "Hoppy", "Citrusy"]
+    },
+    
+        {
+        name: "Stag",
+        strengthCategory: 4.8,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Amber",
+        flavour: ["Toffee", "Malty", "Dry"]
+    },
+    
+        {
+        name: "Dark Conspiracy",
+        strengthCategory: 4.9,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Ruby",
+        flavour: ["Rich", "Smooth"]
+    },
+    
+          {
+        name: "Admiral IPA",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Citrusy", "Hoppy", "Malty"]
+    },
+    
+          {
+        name: "Durdle Door",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Full Bodied", "Caramel", "Dry"]
+    },
+    
+          {
+        name: "Haymaker",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Sweet", "Hoppy", "Fruity"]
+    },
+    
+          {
+        name: "Moose River",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Zesty", "Citrusy", "Fruity"]
+    },
+    
+          {
+        name: "Dissolution IPA",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Pale, Golden",
+        flavour: ["Citrusy", "Hoppy", "Caramel"]
+    },
+    
+          {
+        name: "Ein Stein",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Pale, Blond",
+        flavour: ["Refreshing", "Hoppy", "Citrusy"]
+    },
+    
+    
+          {
+        name: "Old Growler",
+        strengthCategory: 5.0,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Deep Ruby, Black",
+        flavour: ["Roasted", "Smooth", "Fruity"]
+    },
+    
+          {
+        name: "Devon Dumpling",
+        strengthCategory: 5.1,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Smooth", "Sweet", "Fruity"]
+    },
+    
+          {
+        name: "Miners Gold",
+        strengthCategory: 5.1,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Amber",
+        flavour: ["Spicy", "Citrusy", "Refreshing"]
+    },
+    
+          {
+        name: "Pendle Witches Brew",
+        strengthCategory: 5.1,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Amber",
+        flavour: ["Malty", "Spicy", "Fruity"]
+    },
+    
+          {
+        name: "Rutland Beast",
+        strengthCategory: 5.3,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Ruby",
+        flavour: ["Full Bodied", "Citrusy", "Fruity"]
+    },
+    
+          {
+        name: "Gone Fishing",
+        strengthCategory: 5.5,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Ruby",
+        flavour: ["Malty", "Hoppy", "Fruity"]
+    },
+    
+          {
+        name: "Big Chief",
+        strengthCategory: 5.5,
+        strength: "Between 4.6% ABV and 5.5% ABV",
+        colour: "Golden",
+        flavour: ["Bitter", "Honey", "Citrusy"]
     }
 ];
